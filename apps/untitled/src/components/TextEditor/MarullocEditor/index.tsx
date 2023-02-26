c1f3af6e53f002daf94bdefed943917430fd12d4
marulloc <marullo@naver.com>
2023-02-26 21:45:35 +0900

    Rerender cursor 초기화되는 것, 컴포넌트 분리로 우선 해결

--- apps/untitled/src/components/TextEditor/MarullocEditor/index.tsx
+++ apps/untitled/src/components/TextEditor/MarullocEditor/index.tsx
@@ -1,33 +1,49 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
+
+/**
+ * @TODo 1. useRef로 리렌더링 시점에 커서 위치 변하지 않도록 만들기
+ * @TODO 2. 애초에 저장을 인풋할때마다 하지 않기 (쓰로틀링 써서 하거나, 입력이 멈춘지 5초가 지났을 때, 혹은 저장버튼을 눌렀을 때)
+ * @TODO 3. 볼드, 이탤릭, 언더라인 과 같은 (?)텍스트 그 머냐 태그 삽입하는 류의 기능 추가하기
+ * @TODO 4. 코드블락
+ * @TODO 5. 드래거블
+ * @TODO 6. 미디어 삽입 및 사이즈조절
+ * @TODO 7. Layout 변경
+ * @returns
+ */
 
 const MarullocEditor: React.FC = () => {
-    const [content, setContent] = useState(
-        '<p>This is <b>Marulloc Editor </b>!</p>',
-    );
-
-    const handleInput = (evt) => {
-        console.log(evt.target.innerHTML);
-        setContent(evt.target.innerHTML);
-    };
-
-    //
+    const [preview, setPreview] = useState('');
 
     return (
         <div>
-            <button>Bold</button>
-            <button>italic</button>
-            <button>underline</button>
-            <button>highlight</button>
-            <button>code</button>
-            <div
-                contentEditable="true"
-                onInput={handleInput}
-                dangerouslySetInnerHTML={{ __html: content }}
-                //
-                style={{ background: 'white', color: 'red', padding: 20 }}
-            />
+            <EditorCore setContentState={setPreview} />
+
+            <div id="preview" dangerouslySetInnerHTML={{ __html: preview }}></div>
         </div>
     );
 };
 
 export default React.memo(MarullocEditor);
+
+// eslint-disable-next-line react/display-name
+const EditorCore = React.memo(({ setContentState }: any) => {
+    const editorRef = useRef<HTMLDivElement | null>(null);
+    const contentRef = useRef('<p>Marulloc</p>');
+
+    const handleInput = (event) => {
+        contentRef.current = editorRef.current?.innerHTML || '';
+        setContentState(contentRef.current);
+    };
+
+    return (
+        <div
+            ref={editorRef}
+            id="editor"
+            contentEditable
+            // suppressContentEditableWarning
+            onInput={handleInput}
+            dangerouslySetInnerHTML={{ __html: contentRef.current }}
+            style={{ background: 'white', color: 'red', padding: 20 }}
+        />
+    );
+});
