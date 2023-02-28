13205f57cb9aeb02418befc336a4443683bce00f
marulloc <marullo@naver.com>
2023-03-01 00:18:14 +0900

    응답형태 주석으로 메모

--- apps/bi-watcher/src/containers/ContSocketBinance/index.tsx
+++ apps/bi-watcher/src/containers/ContSocketBinance/index.tsx
@@ -1,8 +1,21 @@
 import { useEffect, useRef } from 'react';
 
 const coin = 'btcusdt';
-const END_POINT = `wss://fstream.binance.com/ws/${coin}@trade`;
+const END_POINT = `wss://fstream.binance.com/ws/${coin}@ticker`; //ticker
 
+/**
+ *  {
+    "e": "24hrMiniTicker",  // Event type
+    "E": 1672515782136,     // Event time
+    "s": "BNBBTC",          // Symbol
+    "c": "0.0025",          // Close price
+    "o": "0.0010",          // Open price
+    "h": "0.0025",          // High price
+    "l": "0.0010",          // Low price
+    "v": "10000",           // Total traded base asset volume
+    "q": "18"               // Total traded quote asset volume
+  }
+ */
 type TProps = {
     //
 };
