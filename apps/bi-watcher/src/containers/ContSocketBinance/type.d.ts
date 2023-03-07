7bcba2dbcea08611b4f6b48937052d48bbd37ca0
marulloc <marullo@naver.com>
2023-03-07 23:59:55 +0900

    Typing Binance ticker

--- apps/bi-watcher/src/containers/ContSocketBinance/type.d.ts
+++ apps/bi-watcher/src/containers/ContSocketBinance/type.d.ts
@@ -2,27 +2,27 @@
  * 2023.03.03 기준
  */
 type TBinanceTicker = {
-    A: string; // Ask price'0.00001000' ;
-    B: string; // Bid price '0.05747000';  //
-    C: number; // first trade ID 1678198869332;
-    E: number; // last trade ID 1678198869359; // close time
-    F: 2879031518; // first trade id
-    L: 2885210739; // last trade id
-    O: 1678112469332; // open time
-    P: '-0.318';
-    Q: '0.00351000';
-    a: '22356.21000000';
-    b: '22356.06000000';
-    c: '22356.06000000';
-    e: '24hrTicker';
-    h: '22602.19000000';
-    l: '22291.00000000';
-    n: 6179222;
-    o: '22427.35000000';
-    p: '-71.29000000';
-    q: '4611225807.58941330';
-    s: string; // 해당 ticker의 코인 심볼 'BTCUSDT';
-    v: '205532.31136000';
-    w: '22435.52742183';
-    x: '22427.36000000';
+    e: '24hrTicker'; // Event type
+    E: number; // 123456789; // Event time
+    s: string; // 'BNBBTC'; // Symbol
+    p: string; // '0.0015'; // Price change
+    P: string; // '250.00'; // Price change percent
+    w: string; // '0.0018'; // Weighted average price
+    x: string; // '0.0009'; // First trade(F)-1 price (first trade before the 24hr rolling window)
+    c: string; // '0.0025'; // Last price
+    Q: string; // '10'; // Last quantity
+    b: string; // '0.0024'; // Best bid price
+    B: string; // '10'; // Best bid quantity
+    a: string; // '0.0026'; // Best ask price
+    A: string; // '100'; // Best ask quantity
+    o: string; // '0.0010'; // Open price
+    h: string; // '0.0025'; // High price
+    l: string; // '0.0010'; // Low price
+    v: string; // '10000'; // Total traded base asset volume
+    q: string; // '18'; // Total traded quote asset volume
+    O: number; // 0; // Statistics open time
+    C: number; // 86400000; // Statistics close time
+    F: number; // 0; // First trade ID
+    L: number; // 18150; // Last trade Id
+    n: number; // 18151; // Total number of trades
 };
